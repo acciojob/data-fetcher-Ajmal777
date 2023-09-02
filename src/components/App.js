7122e7293dcbbd445fdbf7080e1caa11066d9408
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-09-02 16:07:51 +0000

    dusra commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,19 @@ import './../styles/App.css';
 import axios from "axios";
 const App = () => {
   const [data, setData] = useState('');
+  const [error, setError] = useState('');
   useEffect(() => {
       axios.get('https://dummyjson.com/products')
-      .then(res => setData(JSON.stringify(res.data)))
-      .catch(error => console.log(error));
+      .then(res => setData(res.data.products))
+      .catch(error => setError(error.data));
   }, []);
 
   return (
     <div>
         <h1>Data Fetched from API</h1>
+        {error && <p>An error occured: {error.message}</p>}
         {
-          data ? <pre>{data}</pre> : <p>Loading...</p>
+          !error && data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>
         }
     </div>
   )
