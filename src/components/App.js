52a915817cf75b270297e6ed70ad5a8e78b534a2
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-09-02 15:54:55 +0000

    pehla commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React , {useEffect, useState} from "react";
 import './../styles/App.css';
-
+import axios from "axios";
 const App = () => {
+  const [data, setData] = useState('');
+  useEffect(() => {
+      axios.get('https://dummyjson.com/products')
+      .then(res => setData(JSON.stringify(res.data)))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Data Fetched from API</h1>
+        {
+          data ? <pre>{data}</pre> : <p>Loading...</p>
+        }
     </div>
   )
 }
